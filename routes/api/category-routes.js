6530493738c7be89e920637f944eb81fcd2f8c9a
fearoffun <480dunce@gmail.com>
2023-07-20 17:51:36 -0700

    finished writing in for category stuff

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -47,10 +47,34 @@ router.post('/', async (req, res) => {
 
 router.put('/:id', async (req, res) => {
   // update a category by its `id` value
+  Category.update(
+    { category_name: req.body.category_name },
+    { returning: true, where: { id: req.params.id } }
+  )
+    .then(function ([rowsUpdate, [updatedCategory]]) {
+      res.json(updatedCategory)
+    })
+    .catch(next)
 });
 
 router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
+  try {
+    const categoryData = await Category.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!categoryData) {
+      res.status(404).json({ message: 'No category found with that id.' });
+      return;
+    }
+
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
